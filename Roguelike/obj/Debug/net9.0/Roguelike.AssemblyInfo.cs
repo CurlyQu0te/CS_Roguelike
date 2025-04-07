ebf2741146f264182b1a6877d1d5b6e9915f7d5c
CurlyQu0te <sorozc15@asu.edu>
2025-04-07 16:58:16 -0700

    Added more consoles for better UI

--- Roguelike/obj/Debug/net9.0/Roguelike.AssemblyInfo.cs
+++ Roguelike/obj/Debug/net9.0/Roguelike.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Roguelike")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a9d5d1f64769713c32bd04fed99bebac45853f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9136bdf0512590e0bc5a80d99cb81f6a4c72034")]
 [assembly: System.Reflection.AssemblyProductAttribute("Roguelike")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Roguelike")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
