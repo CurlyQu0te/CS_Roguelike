e9b7678e9202d6c1937f172d36761358a343bb14
CurlyQu0te <sorozc15@asu.edu>
2025-04-07 19:41:38 -0700

    Added Colors and Swatch

---
+++ Roguelike/Core/Swatch.cs
@@ -0,0 +1,47 @@
+using RLNET;
+
+namespace RogueLike {
+    public class Swatch {
+        public static RLColor PrimaryLightest = new RLColor( 126, 163, 132 );
+        public static RLColor PrimaryLighter = new RLColor( 96, 144, 104);
+        public static RLColor Primary = new RLColor( 71, 127, 81 );
+        public static RLColor PrimaryDarker = new RLColor( 51, 108, 61);
+        public static RLColor PrimaryDarkest = new RLColor( 31, 100, 43 );
+
+        public static RLColor SecondaryLightest = new RLColor( 101, 127, 131 );
+        public static RLColor SecondaryLighter = new RLColor( 78, 110, 115 );
+        public static RLColor Secondary = new RLColor( 58, 97, 102 );
+        public static RLColor SecondaryDarker = new RLColor( 42, 81, 86 );
+        public static RLColor SecondaryDarkest = new RLColor( 26, 73, 80 );
+
+        public static RLColor TertiaryLightest = new RLColor( 213, 188, 165 );
+        public static RLColor TertiaryLighter = new RLColor( 188, 155, 125 );
+        public static RLColor Tertiary = new RLColor( 167, 128, 93);
+        public static RLColor TertiaryDarker = new RLColor( 141, 103, 67 );
+        public static RLColor TertiaryDarkest = new RLColor( 130, 83, 40 );
+
+        public static RLColor QuaternaryLightest = new RLColor( 213, 168, 165 );
+        public static RLColor QuaternaryLighter = new RLColor( 188, 130, 125 );
+        public static RLColor Quaternary = new RLColor( 167, 98, 93 );
+        public static RLColor QuaternaryDarker = new RLColor( 141, 72, 67 );
+        public static RLColor QuaternaryDarkest = new RLColor( 130, 47, 40 );
+
+        public static RLColor DbDark = new RLColor( 20, 12, 28 );
+        public static RLColor DbDarkBlood = new RLColor( 68, 36, 52 );
+        public static RLColor DbDarkWater = new RLColor( 48, 52, 109 );
+        public static RLColor DbDarkStone = new RLColor( 78, 74, 78 );
+        public static RLColor DbDarkWood = new RLColor( 133, 76, 48);
+        public static RLColor DbDarkGrass = new RLColor( 52, 101, 36 );
+        public static RLColor DbBlood = new RLColor( 208, 70, 72 );
+        public static RLColor DbStone = new RLColor( 117, 113, 97 );
+        public static RLColor DbWater = new RLColor( 89, 125, 206 );
+        public static RLColor DbWood = new RLColor( 210, 125, 44 );
+        public static RLColor DbMetal = new RLColor( 133, 149, 161 );
+        public static RLColor DbGrass = new RLColor( 109, 170, 44 );
+        public static RLColor DbSkin = new RLColor( 210, 170, 153);
+        public static RLColor DbSky = new RLColor( 109, 194, 202 );
+        public static RLColor DbHoly = new RLColor( 218, 212, 94 );
+        public static RLColor DbLight = new RLColor( 222, 238, 214 );
+
+    }
+}
